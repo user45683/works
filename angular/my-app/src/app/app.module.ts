ff3eb9def649a9876c37d7bef16994ba2edbfaa8
user45683 <gm2308483@gmail.com>
2022-02-05 00:58:15 +0300

    &

--- angular/my-app/src/app/app.module.ts
+++ angular/my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
